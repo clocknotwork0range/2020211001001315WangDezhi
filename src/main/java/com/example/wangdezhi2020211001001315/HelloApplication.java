4017e5a8b6bd12898d8f088fb05492c33395c4b8
王德智 <2861153556@qq.com>
2022-03-01 17:23:16 +0800

    First Commit

---
+++ src/main/java/com/example/wangdezhi2020211001001315/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.example.wangdezhi2020211001001315;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
